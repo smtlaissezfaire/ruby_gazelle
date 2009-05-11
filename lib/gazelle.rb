7dea2283889263a9f8dc55f1ff75e76cc0944360
Scott Taylor <scott@railsnewbie.com>
2009-05-10 22:14:06 -0400

    Move Gazelle::Parser into lib/gazelle/parser.rb.  Use the using gem

--- lib/gazelle.rb
+++ lib/gazelle.rb
@@ -1,32 +1,10 @@
 
+require "rubygems"
+require "using"
+
 module Gazelle
-  class Parser
-    def initialize(filename)
-      file = expand_path(filename)
-      raise(Errno::ENOENT) unless File.exists?(file)
-      
-      @filename = file
-      @on = {}
-    end
-    
-    def on(sym, &block)
-      @on[sym] = block
-    end
-    
-    def rules(&block)
-      instance_eval(&block)
-    end
-    
-  private
-  
-    def run_rule(name, str)
-      @on[name.to_sym].call(str)
-    end
-  
-    def expand_path(filename)
-      File.expand_path(filename)
-    end
-  end
+  extend Using
+  using :Parser
 end
 
 require File.dirname(__FILE__) + "/gazelle.bundle"
---
+++ lib/gazelle/parser.rb
@@ -0,0 +1,29 @@
+module Gazelle
+  class Parser
+    def initialize(filename)
+      file = expand_path(filename)
+      raise(Errno::ENOENT) unless File.exists?(file)
+      
+      @filename = file
+      @on = {}
+    end
+    
+    def on(sym, &block)
+      @on[sym] = block
+    end
+    
+    def rules(&block)
+      instance_eval(&block)
+    end
+    
+  private
+  
+    def run_rule(name, str)
+      @on[name.to_sym].call(str)
+    end
+  
+    def expand_path(filename)
+      File.expand_path(filename)
+    end
+  end
+end
